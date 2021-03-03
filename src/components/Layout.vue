0a89850008fd86487eacf64e96dc9fa771cb570a
lushengqin <2015941587@qq.com>
2021-03-03 10:41:58 +0800

    icon svg

---
+++ src/components/Layout.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="nav">
+        <div class="content">
+            <slot></slot>
+        </div>
+        <Nav></Nav>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Layout'
+    };
+</script>
+
+<style lang="scss" scoped>
+    .nav{
+        display: flex;
+        flex-direction: column;
+        max-width: 100vw;
+        height: 100vh;
+        .content{
+            width: calc(100% - 40px);
+            margin: 20px;
+            flex: 1;
+            align-self: flex-start;
+            overflow-y: auto;
+        }
+
+    }
+</style>
