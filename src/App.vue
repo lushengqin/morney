8293255fbb819e3dd8d7b073fd3454f1e6c96ef4
lushengqin <2015941587@qq.com>
2021-03-03 15:01:10 +0800

    <Icon>组件

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <style lang="scss">
-  body {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,28 +1,55 @@
 <template>
     <div id="nav">
-        <router-link class="nav-li" to="./Labels">明细</router-link>
-        <router-link class="nav-li" to="./Money">记账</router-link>
-        <router-link class="nav-li" to="./Statistic">账单</router-link>
+        <router-link class="nav-li" to="./Labels">
+            <Icon name="labels"></Icon>
+            <div class="title">明细</div>
+        </router-link>
+        <router-link class="nav-li" to="./Money">
+            <Icon name="money"></Icon>
+            <div class="title">记账</div>
+        </router-link>
+        <router-link class="nav-li" to="./Statistic">
+            <Icon name="statistic"></Icon>
+            <div class="title">账单</div>
+        </router-link>
     </div>
 </template>
 
 <script lang="ts">
+    //引入全局icon
+    const importAll = (requireContext: __WebpackModuleApi.RequireContext)=>requireContext.keys().forEach(requireContext);
+    try {
+        importAll(require.context('../assets/icons',true,/\.svg$/))
+    }catch (error) {
+        console.log(error)
+    }
+
+    import Icon from '@/components/Icon.vue';
     export default {
-        name: 'Nav'
+        name: 'Nav',
+        components: {Icon}
     };
 </script>
 
 <style lang="scss" scoped>
     #nav {
         background-color: #f8f8f8;
-        height: 100px;
         display: flex;
-
+        justify-content: center;
+        text-align: center;
+        padding-top: 5px;
+        padding-bottom: 5px;
         .nav-li {
             flex: 1;
             text-align: center;
             align-self: flex-end;
             text-decoration: none;
+            font-size: 14px;
+            svg{
+                height: 18px;
+                width: 18px;
+                font-size: 18px;
+            }
         }
     }
 </style>
